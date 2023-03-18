ab8fd63511166977cf9c5c914b2bc5c535b8ee59
JDawg1993 <J0rdan.d3an1993@gmail.com>
2023-03-18 17:06:08 +0000

    Updated Packages, Allowed Access to steam

--- Assets/Scripts/Cameras/Minimap.cs
+++ Assets/Scripts/Cameras/Minimap.cs
@@ -15,7 +15,8 @@ public class Minimap : MonoBehaviour, IPointerDownHandler, IDragHandler
 
     private void Update() {
         if(playerCameraTransform != null) {return;}
-        if(NetworkClient.connection.identity == null) {return;}
+
+        if(NetworkClient.connection?.identity == null) {return;}
         playerCameraTransform = NetworkClient.connection.identity
             .GetComponent<RTSPlayer>().GetCameraTransform();
     }
